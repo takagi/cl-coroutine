7109bd19207d9166de35b4eef832ae66135c83a3
Masayuki Takagi <kamonama@gmail.com>
2014-05-28 16:25:09 +0900

    modify DEFCOROUTINE macro to return the name of coroutine to be defined.

--- src/cl-coroutine.lisp
+++ src/cl-coroutine.lisp
@@ -27,54 +27,58 @@
 
 (defun defcoroutine/arg (name arg body)
   (alexandria:with-gensyms (cont)
-    `(setf (get ',name 'make-coroutine)
-           #'(lambda ()
-               (let (,cont)
-                 #'(lambda (,arg)
-                     (declare (ignorable ,arg))
-                     (if ,cont
-                       (funcall ,cont ,arg)
-                       (cl-cont:with-call/cc
-                         (macrolet ((yield (&optional result)
-                                      (let ((cc (gensym)))
-                                        `(setf ,',arg
-                                               (cl-cont:let/cc ,cc
-                                                 (setf ,',cont ,cc)
-                                                 ,result))))
-                                    (coexit (&optional result)
-                                      `(cl-cont:let/cc _
-                                         (declare (ignorable _))
-                                         (setf ,',cont
-                                               #'(lambda (_)
-                                                   (declare (ignorable _))
-                                                   (values)))
-                                         ,result)))
-                           ,@body
-                           (coexit nil))))))))))
+    `(progn
+       (setf (get ',name 'make-coroutine)
+             #'(lambda ()
+                 (let (,cont)
+                   #'(lambda (,arg)
+                       (declare (ignorable ,arg))
+                       (if ,cont
+                         (funcall ,cont ,arg)
+                         (cl-cont:with-call/cc
+                           (macrolet ((yield (&optional result)
+                                        (let ((cc (gensym)))
+                                          `(setf ,',arg
+                                                 (cl-cont:let/cc ,cc
+                                                   (setf ,',cont ,cc)
+                                                   ,result))))
+                                      (coexit (&optional result)
+                                        `(cl-cont:let/cc _
+                                           (declare (ignorable _))
+                                           (setf ,',cont
+                                                 #'(lambda (_)
+                                                     (declare (ignorable _))
+                                                     (values)))
+                                           ,result)))
+                             ,@body
+                             (coexit nil))))))))
+       ',name)))
 
 (defun defcoroutine/no-arg (name body)
   (alexandria:with-gensyms (cont)
-    `(setf (get ',name 'make-coroutine)
-           #'(lambda ()
-               (let (,cont)
-                 #'(lambda ()
-                     (if ,cont
-                       (funcall ,cont)
-                       (cl-cont:with-call/cc
-                         (macrolet ((yield (&optional result)
-                                      (let ((cc (gensym)))
-                                        `(cl-cont:let/cc ,cc
-                                           (setf ,',cont ,cc)
+    `(progn
+       (setf (get ',name 'make-coroutine)
+             #'(lambda ()
+                 (let (,cont)
+                   #'(lambda ()
+                       (if ,cont
+                         (funcall ,cont)
+                         (cl-cont:with-call/cc
+                           (macrolet ((yield (&optional result)
+                                        (let ((cc (gensym)))
+                                          `(cl-cont:let/cc ,cc
+                                             (setf ,',cont ,cc)
+                                             ,result)))
+                                      (coexit (&optional result)
+                                        `(cl-cont:let/cc _
+                                           (declare (ignorable _))
+                                           (setf ,',cont
+                                                 #'(lambda ()
+                                                     (values)))
                                            ,result)))
-                                    (coexit (&optional result)
-                                      `(cl-cont:let/cc _
-                                         (declare (ignorable _))
-                                         (setf ,',cont
-                                               #'(lambda ()
-                                                   (values)))
-                                         ,result)))
-                           ,@body
-                           (coexit nil))))))))))
+                             ,@body
+                             (coexit nil))))))))
+       ',name)))
 
 
 ;;;
